485bd77e1aaa761fd1273f1180ae0d74884ef307
parkerd3 <parker.donaldson3@gmail.com>
2026-04-23 14:49:58 -0600

    Finished assignment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string pbd_first = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string pbd_last = Console.ReadLine();
+
+        Console.WriteLine("");
+        string pbd_agent_name = 
+            pbd_last + ", " + pbd_first + " " + pbd_last;
+        Console.WriteLine($"Your name is {pbd_agent_name}.");
     }
 }
