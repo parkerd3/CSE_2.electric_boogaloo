b1ce2607b4b3c892ba0f20df539646cbf4a1efda
parkerd3 <parker.donaldson3@gmail.com>
2026-05-01 23:59:14 -0600

    assignment finished

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.Net.Quic;
+using System.Numerics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Hello there friend,");
+        Console.WriteLine("enter a list of numbers, type 0 when finished");
+        
+        List<int> numbers = new List<int>();
+        int num = 0;
+        do
+        {
+            Console.Write("Enter number: ");
+            string input = Console.ReadLine();
+            num = int.Parse(input);
+            if (num != 0)
+            {
+                numbers.Add(num);
+            }
+        } while (num != 0);
+
+        // Sum
+        int total = 0;
+        int max = 0;
+        foreach (int number in numbers)
+        {
+            total += number;
+            if (number >= max)
+            {
+                max = number;
+            }
+        }
+        Console.Write($"The sum is: {total}");
+        // Ave
+        float average = 0;
+        int qty = numbers.Count();
+        average = total / qty;
+        Console.Write($"The average value is: {average}");
+        // Max
+        Console.Write($"The maximum value is: {max}");
     }
 }
