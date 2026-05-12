b8dee1cec366ebe6df76bd91db7ba762e28c6504
parkerd3 <parker.donaldson3@gmail.com>
2026-05-12 13:25:58 -0600

    Wrote the most hilarious joke of all time

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,16 @@
 using System;
+using System.IO;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        string fileName = "Bad_joke_Dad_joke.txt";
+
+        using (StreamWriter outputFile = new StreamWriter(fileName))
+        {
+            outputFile.WriteLine("Why did the chicken cross the road? \n");
+            outputFile.WriteLine("To get to the other side");
+        }
     }
 }
