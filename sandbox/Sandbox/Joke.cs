8d8b2fba6628a61b3850105edc0c8311cc711fd4
parkerd3 <parker.donaldson3@gmail.com>
2026-05-13 01:48:02 -0600

    Cleaning up any other changes made

--- sandbox/Sandbox/Joke.cs
+++ sandbox/Sandbox/Joke.cs
@@ -1,4 +1,23 @@
-// public class Joke
-// {
-//     public 
-// }
+public class Joke
+{
+    public string _file_name;
+    public string _joke;
+
+    
+    
+    // Functions.
+
+    public void Write(string joke)
+    {
+        Console.WriteLine("Enter your joke:");
+        joke = Console.ReadLine();
+        Console.WriteLine("Enter filename to save to:");
+        _file_name = Console.ReadLine();
+
+        using (StreamWriter outputFile = new StreamWriter(_file_name))
+        {
+            outputFile.WriteLine("Why did the chicken turn the page?\n");
+            outputFile.WriteLine("To get to the other side");
+        }
+    }
+}
