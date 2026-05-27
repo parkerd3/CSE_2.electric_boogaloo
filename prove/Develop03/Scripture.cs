4adde893df2a7c9cc42d62fa40e97898fe23486d
parkerd3 <parker.donaldson3@gmail.com>
2026-05-26 19:54:46 -0600

    Building up the scripture class. Need to implement the obscure method so that it always results in a word being hidden

--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -1,12 +1,46 @@
+using System.ComponentModel.DataAnnotations;
+
 public class Scripture
 {
   private Reference reference;
-  private Passage word;
+  private List<Passage> passages = new();
 
   public Scripture(string bookTitle, string chpVerses)
   {
     reference = new Reference(bookTitle, chpVerses);
-    word = new Passage(reference);
+    foreach (int i in reference.Verses())
+    {
+      int chp = reference.Chapter();
+      passages.Add( new Passage(bookTitle, chp, i) );
+    }
   }
 
+  public override string ToString()
+  {
+    string display = reference + "\n";
+
+    foreach (Passage passage in passages)
+    {
+      display += passage + "\n";
+    }
+
+    return display;
+  }
+
+  public void Obscure()
+  {
+    int hideCount = int.Min(3, passages.Count()/2);
+    Random random = new();
+    for (int i = 0; i < hideCount; i++)
+    {
+      int idx = random.Next(hideCount);
+      // TODO: Make it so that this loops until every word is hidden, or until
+      // Passage.Obscure() has been successfully called hideCount times.
+    }
+  }
+
+  public void Restore()
+  {
+    
+  }
 }
