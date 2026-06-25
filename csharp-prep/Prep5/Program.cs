14e6403c87a729cac57f2c1e64f6db73729bb10f
parkerd3 <parker.donaldson3@gmail.com>
2026-06-25 17:59:21 -0600

    finished prep activity

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,17 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+  static void Main(string[] args)
+  {
+    List<Shape> shapes = new List<Shape>();
+
+    shapes.Add(new Square(13, "Chartreuse"));
+    shapes.Add(new Rectangle(3.2, 4.6, "Fuschia"));
+    shapes.Add(new Circle(2.3, "Cerulean"));
+
+    foreach (Shape shape in shapes)
     {
-        Console.WriteLine("Hello Prep5 World!");
+      Console.WriteLine($"{shape.GetArea()}, {shape.GetColor()}");
     }
+  }
 }
---
+++ csharp-prep/Prep5/Square.cs
@@ -0,0 +1,7 @@
+public class Square : Rectangle
+{
+  public Square(double side, string color) : base(side, side, color)
+  {
+    
+  }
+}
